c38983f5ff6e562cd029467f2e37d42079b006b5
Dustin Sallings <dustin@spy.net>
2012-01-12 19:06:15 -0800

    Less dumb interface.

--- bytes.go
+++ bytes.go
@@ -6,25 +6,49 @@ import (
 )
 
 const (
-	Byte  = 1
-	KByte = Byte * 1024
-	MByte = KByte * 1024
-	GByte = MByte * 1024
-	TByte = GByte * 1024
-	PByte = TByte * 1024
-	EByte = PByte * 1024
+	Byte   = 1
+	KiByte = Byte * 1024
+	MiByte = KiByte * 1024
+	GiByte = MiByte * 1024
+	TiByte = GiByte * 1024
+	PiByte = TiByte * 1024
+	EiByte = PiByte * 1024
 )
 
-func log1024(n float64) float64 {
-	return math.Log(n) / math.Log(1024)
+const (
+	IByte = 1
+	KByte = IByte * 1000
+	MByte = KByte * 1000
+	GByte = MByte * 1000
+	TByte = GByte * 1000
+	PByte = TByte * 1000
+	EByte = PByte * 1000
+)
+
+type Bytes uint64
+
+type IBytes uint64
+
+func logn(n, b float64) float64 {
+	return math.Log(n) / math.Log(b)
 }
 
-func Bytes(s uint64) string {
+func humanateBytes(s uint64, base float64, sizes []string) string {
 	if s == 0 {
 		return "0B"
 	}
-	sizes := []string{"B", "KB", "MB", "GB", "TB", "PB", "EB"}
-	e := math.Floor(log1024(float64(s)))
+	e := math.Floor(logn(float64(s), base))
 	suffix := sizes[int(e)]
-	return fmt.Sprintf("%.0f%s", float64(s)/math.Pow(1024, math.Floor(e)), suffix)
+	return fmt.Sprintf("%.0f%s", float64(s)/math.Pow(base, math.Floor(e)), suffix)
+
+}
+
+func (s Bytes) String() string {
+	sizes := []string{"B", "KB", "MB", "GB", "TB", "PB", "EB"}
+	return humanateBytes(uint64(s), 1000, sizes)
+}
+
+func (s IBytes) String() string {
+	sizes := []string{"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB"}
+	return humanateBytes(uint64(s), 1024, sizes)
 }
--- bytes_test.go
+++ bytes_test.go
@@ -11,39 +11,77 @@ func assert(t *testing.T, name string, got interface{}, expected interface{}) {
 }
 
 func TestBytes(t *testing.T) {
-	assert(t, "bytes(0)", Bytes(0), "0B")
-	assert(t, "bytes(1)", Bytes(1), "1B")
-	assert(t, "bytes(803)", Bytes(803), "803B")
-	assert(t, "bytes(1023)", Bytes(1023), "1023B")
+	assert(t, "bytes(0)", Bytes(0).String(), "0B")
+	assert(t, "bytes(1)", Bytes(1).String(), "1B")
+	assert(t, "bytes(803)", Bytes(803).String(), "803B")
+	assert(t, "bytes(999)", Bytes(999).String(), "999B")
 }
 
 func TestK(t *testing.T) {
-	assert(t, "bytes(1024)", Bytes(1024), "1KB")
-	assert(t, "bytes(1MB - 1)", Bytes(MByte-Byte), "1024KB")
+	assert(t, "bytes(1024)", Bytes(1024).String(), "1KB")
+	assert(t, "bytes(1MB - 1)", Bytes(MByte-Byte).String(), "1000KB")
 }
 
 func TestM(t *testing.T) {
-	assert(t, "bytes(1MB)", Bytes(1024*1024), "1MB")
-	assert(t, "bytes(1GB - 1K)", Bytes(GByte-KByte), "1024MB")
+	assert(t, "bytes(1MB)", Bytes(1024*1024).String(), "1MB")
+	assert(t, "bytes(1GB - 1K)", Bytes(GByte-KByte).String(), "1000MB")
 }
 
 func TestG(t *testing.T) {
-	assert(t, "bytes(1GB)", Bytes(GByte), "1GB")
-	assert(t, "bytes(1TB - 1M)", Bytes(TByte-MByte), "1024GB")
+	assert(t, "bytes(1GB)", Bytes(GByte).String(), "1GB")
+	assert(t, "bytes(1TB - 1M)", Bytes(TByte-MByte).String(), "1000GB")
 }
 
 func TestT(t *testing.T) {
-	assert(t, "bytes(1TB)", Bytes(TByte), "1TB")
-	assert(t, "bytes(1PB - 1T)", Bytes(PByte-TByte), "1023TB")
+	assert(t, "bytes(1TB)", Bytes(TByte).String(), "1TB")
+	assert(t, "bytes(1PB - 1T)", Bytes(PByte-TByte).String(), "999TB")
 }
 
 func TestP(t *testing.T) {
-	assert(t, "bytes(1PB)", Bytes(PByte), "1PB")
-	assert(t, "bytes(1PB - 1T)", Bytes(EByte-PByte), "1023PB")
+	assert(t, "bytes(1PB)", Bytes(PByte).String(), "1PB")
+	assert(t, "bytes(1PB - 1T)", Bytes(EByte-PByte).String(), "999PB")
 }
 
 func TestE(t *testing.T) {
-	assert(t, "bytes(1EB)", Bytes(EByte), "1EB")
+	assert(t, "bytes(1EB)", Bytes(EByte).String(), "1EB")
 	// Overflows.
 	// assert(t, "bytes(1EB - 1P)", Bytes((KByte*EByte)-PByte), "1023EB")
+}
+
+func TestIIBytes(t *testing.T) {
+	assert(t, "bytes(0)", IBytes(0).String(), "0B")
+	assert(t, "bytes(1)", IBytes(1).String(), "1B")
+	assert(t, "bytes(803)", IBytes(803).String(), "803B")
+	assert(t, "bytes(1023)", IBytes(1023).String(), "1023B")
+}
+
+func TestIK(t *testing.T) {
+	assert(t, "bytes(1024)", IBytes(1024).String(), "1KiB")
+	assert(t, "bytes(1MB - 1)", IBytes(MiByte-IByte).String(), "1024KiB")
+}
+
+func TestIM(t *testing.T) {
+	assert(t, "bytes(1MB)", IBytes(1024*1024).String(), "1MiB")
+	assert(t, "bytes(1GB - 1K)", IBytes(GiByte-KiByte).String(), "1024MiB")
+}
+
+func TestIG(t *testing.T) {
+	assert(t, "bytes(1GB)", IBytes(GiByte).String(), "1GiB")
+	assert(t, "bytes(1TB - 1M)", IBytes(TiByte-MiByte).String(), "1024GiB")
+}
+
+func TestIT(t *testing.T) {
+	assert(t, "bytes(1TB)", IBytes(TiByte).String(), "1TiB")
+	assert(t, "bytes(1PB - 1T)", IBytes(PiByte-TiByte).String(), "1023TiB")
+}
+
+func TestIP(t *testing.T) {
+	assert(t, "bytes(1PB)", IBytes(PiByte).String(), "1PiB")
+	assert(t, "bytes(1PB - 1T)", IBytes(EiByte-PiByte).String(), "1023PiB")
+}
+
+func TestIE(t *testing.T) {
+	assert(t, "bytes(1EiB)", IBytes(EiByte).String(), "1EiB")
+	// Overflows.
+	// assert(t, "bytes(1EB - 1P)", IBytes((KIByte*EIByte)-PiByte), "1023EB")
 }
