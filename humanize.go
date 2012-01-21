06b67a146402e218ea0eddd70901f4d0b49ef1c4
Dustin Sallings <dustin@spy.net>
2012-01-21 10:50:04 -0800

    Added some package docs.

---
+++ humanize.go
@@ -0,0 +1,8 @@
+/*
+Units for humans.
+
+Durations can be turned into strings such as "3 days ago", numbers
+representing sizes like 82854982 into useful strings like, "83MB" or
+"79MiB" (whichever you prefer).
+*/
+package humanize
