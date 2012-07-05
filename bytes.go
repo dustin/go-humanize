5257854bbab5614f4dd21625516644087a4bb2f3
Dustin Sallings <dustin@spy.net>
2012-07-05 15:31:23 -0700

    Include tenths in all < 10 cases except for Bytes.

--- bytes.go
+++ bytes.go
@@ -33,14 +33,14 @@ func logn(n, b float64) float64 {
 }
 
 func humanateBytes(s uint64, base float64, sizes []string) string {
-	if s == 0 {
-		return "0B"
+	if s < 10 {
+		return fmt.Sprintf("%dB", s)
 	}
 	e := math.Floor(logn(float64(s), base))
 	suffix := sizes[int(e)]
 	val := float64(s) / math.Pow(base, math.Floor(e))
 	f := "%.0f"
-	if val < 10 && int((val-math.Floor(val))*10) != 0 {
+	if val < 10 {
 		f = "%.1f"
 	}
 
--- bytes_test.go
+++ bytes_test.go
@@ -18,32 +18,32 @@ func TestBytes(t *testing.T) {
 }
 
 func TestK(t *testing.T) {
-	assert(t, "bytes(1024)", Bytes(1024), "1KB")
+	assert(t, "bytes(1024)", Bytes(1024), "1.0KB")
 	assert(t, "bytes(1MB - 1)", Bytes(MByte-Byte), "1000KB")
 }
 
 func TestM(t *testing.T) {
-	assert(t, "bytes(1MB)", Bytes(1024*1024), "1MB")
+	assert(t, "bytes(1MB)", Bytes(1024*1024), "1.0MB")
 	assert(t, "bytes(1GB - 1K)", Bytes(GByte-KByte), "1000MB")
 }
 
 func TestG(t *testing.T) {
-	assert(t, "bytes(1GB)", Bytes(GByte), "1GB")
+	assert(t, "bytes(1GB)", Bytes(GByte), "1.0GB")
 	assert(t, "bytes(1TB - 1M)", Bytes(TByte-MByte), "1000GB")
 }
 
 func TestT(t *testing.T) {
-	assert(t, "bytes(1TB)", Bytes(TByte), "1TB")
+	assert(t, "bytes(1TB)", Bytes(TByte), "1.0TB")
 	assert(t, "bytes(1PB - 1T)", Bytes(PByte-TByte), "999TB")
 }
 
 func TestP(t *testing.T) {
-	assert(t, "bytes(1PB)", Bytes(PByte), "1PB")
+	assert(t, "bytes(1PB)", Bytes(PByte), "1.0PB")
 	assert(t, "bytes(1PB - 1T)", Bytes(EByte-PByte), "999PB")
 }
 
 func TestE(t *testing.T) {
-	assert(t, "bytes(1EB)", Bytes(EByte), "1EB")
+	assert(t, "bytes(1EB)", Bytes(EByte), "1.0EB")
 	// Overflows.
 	// assert(t, "bytes(1EB - 1P)", Bytes((KByte*EByte)-PByte), "1023EB")
 }
@@ -56,32 +56,32 @@ func TestIIBytes(t *testing.T) {
 }
 
 func TestIK(t *testing.T) {
-	assert(t, "bytes(1024)", IBytes(1024), "1KiB")
+	assert(t, "bytes(1024)", IBytes(1024), "1.0KiB")
 	assert(t, "bytes(1MB - 1)", IBytes(MiByte-IByte), "1024KiB")
 }
 
 func TestIM(t *testing.T) {
-	assert(t, "bytes(1MB)", IBytes(1024*1024), "1MiB")
+	assert(t, "bytes(1MB)", IBytes(1024*1024), "1.0MiB")
 	assert(t, "bytes(1GB - 1K)", IBytes(GiByte-KiByte), "1024MiB")
 }
 
 func TestIG(t *testing.T) {
-	assert(t, "bytes(1GB)", IBytes(GiByte), "1GiB")
+	assert(t, "bytes(1GB)", IBytes(GiByte), "1.0GiB")
 	assert(t, "bytes(1TB - 1M)", IBytes(TiByte-MiByte), "1024GiB")
 }
 
 func TestIT(t *testing.T) {
-	assert(t, "bytes(1TB)", IBytes(TiByte), "1TiB")
+	assert(t, "bytes(1TB)", IBytes(TiByte), "1.0TiB")
 	assert(t, "bytes(1PB - 1T)", IBytes(PiByte-TiByte), "1023TiB")
 }
 
 func TestIP(t *testing.T) {
-	assert(t, "bytes(1PB)", IBytes(PiByte), "1PiB")
+	assert(t, "bytes(1PB)", IBytes(PiByte), "1.0PiB")
 	assert(t, "bytes(1PB - 1T)", IBytes(EiByte-PiByte), "1023PiB")
 }
 
 func TestIE(t *testing.T) {
-	assert(t, "bytes(1EiB)", IBytes(EiByte), "1EiB")
+	assert(t, "bytes(1EiB)", IBytes(EiByte), "1.0EiB")
 	// Overflows.
 	// assert(t, "bytes(1EB - 1P)", IBytes((KIByte*EIByte)-PiByte), "1023EB")
 }
