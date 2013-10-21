f27b846295f451532825bcf419940225a0349186
Dustin Sallings <dustin@spy.net>
2013-10-21 01:26:28 -0700

    Shove commas into big.Ints

--- comma.go
+++ comma.go
@@ -1,6 +1,7 @@
 package humanize
 
 import (
+	"math/big"
 	"strconv"
 	"strings"
 )
@@ -30,5 +31,36 @@ func Comma(v int64) string {
 		j--
 	}
 	parts[j] = strconv.Itoa(int(v))
+	return sign + strings.Join(parts[j:len(parts)], ",")
+}
+
+// BigComma produces a string form of the given big.Int in base 10
+// with commas after every three orders of magnitude.
+func BigComma(b *big.Int) string {
+	sign := ""
+	if b.Sign() < 0 {
+		sign = "-"
+		b.Abs(b)
+	}
+
+	athousand := big.NewInt(1000)
+	c := (&big.Int{}).Set(b)
+	_, m := oom(c, athousand, 2e10)
+	parts := make([]string, m+1)
+	j := len(parts) - 1
+
+	mod := &big.Int{}
+	for b.Cmp(athousand) >= 0 {
+		b.DivMod(b, athousand, mod)
+		parts[j] = strconv.FormatInt(mod.Int64(), 10)
+		switch len(parts[j]) {
+		case 2:
+			parts[j] = "0" + parts[j]
+		case 1:
+			parts[j] = "00" + parts[j]
+		}
+		j--
+	}
+	parts[j] = strconv.Itoa(int(b.Int64()))
 	return sign + strings.Join(parts[j:len(parts)], ",")
 }
