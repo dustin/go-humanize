12f1745e821f3d6e19227d5c83299b8c856fc70f
Dustin Sallings <dustin@spy.net>
2014-07-07 21:07:09 -0700

    Got rid of siNumber in favor of just two results

--- si.go
+++ si.go
@@ -25,17 +25,10 @@ var siPrefixTable = map[float64]string{
 	24:  "Y", // yotta
 }
 
-// siNumber holds computed Value and Prefix.
-// can be used with fmt.Sprintf for arbitrary formatting.
-type siNumber struct {
-	Value  float64
-	Prefix string
-}
-
-// NewSI returns a {Value, Prefix} struct for further formatting
-func newSI(input float64) siNumber {
+// NewSI returns a value and prefix for further formatting
+func newSI(input float64) (float64, string) {
 	if input == 0 {
-		return siNumber{0, ""}
+		return 0, ""
 	}
 	exponent := math.Floor(logn(input, 10))
 	exponent = math.Floor(exponent/3) * 3
@@ -50,7 +43,7 @@ func newSI(input float64) siNumber {
 	}
 
 	prefix := siPrefixTable[exponent]
-	return siNumber{value, prefix}
+	return value, prefix
 }
 
 // SI returns a string with default formatting
@@ -58,6 +51,6 @@ func newSI(input float64) siNumber {
 // e.g. SI(1000000, B) -> 1MB
 // e.g. SI(2.2345e-12, "F") -> 2.2345pF
 func SI(input float64, unit string) string {
-	si := newSI(input)
-	return fmt.Sprintf("%s%s%s", Ftoa(si.Value), si.Prefix, unit)
+	value, prefix := newSI(input)
+	return fmt.Sprintf("%s%s%s", Ftoa(value), prefix, unit)
 }
