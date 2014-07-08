7586f6b84d59cd3979a5060ca843bdbb123dff6a
Dustin Sallings <dustin@spy.net>
2014-07-07 21:05:08 -0700

    Get rid of regex and fmt from Ftoa

--- ftoa.go
+++ ftoa.go
@@ -1,14 +1,23 @@
 package humanize
 
-import (
-	"fmt"
-	"regexp"
-)
+import "strconv"
 
-var trailingZerosRegex = regexp.MustCompile(`\.?0+$`)
+func stripTrailingZeros(s string) string {
+	offset := len(s) - 1
+	for offset > 0 {
+		if s[offset] == '.' {
+			offset--
+			break
+		}
+		if s[offset] != '0' {
+			break
+		}
+		offset--
+	}
+	return s[:offset+1]
+}
 
 // Ftoa converts a float to a string with no trailing zeros.
 func Ftoa(num float64) string {
-	str := fmt.Sprintf("%f", num)
-	return trailingZerosRegex.ReplaceAllString(str, "")
+	return stripTrailingZeros(strconv.FormatFloat(num, 'f', 6, 64))
 }
--- ftoa_test.go
+++ ftoa_test.go
@@ -1,6 +1,9 @@
 package humanize
 
 import (
+	"fmt"
+	"regexp"
+	"strconv"
 	"testing"
 )
 
@@ -12,4 +15,41 @@ func TestFtoa(t *testing.T) {
 		{"2.02", Ftoa(2.02), "2.02"},
 		{"200.02", Ftoa(200.02), "200.02"},
 	}.validate(t)
+}
+
+func BenchmarkFtoaRegexTrailing(b *testing.B) {
+	trailingZerosRegex := regexp.MustCompile(`\.?0+$`)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		trailingZerosRegex.ReplaceAllString("2.00000", "")
+		trailingZerosRegex.ReplaceAllString("2.0000", "")
+		trailingZerosRegex.ReplaceAllString("2.000", "")
+		trailingZerosRegex.ReplaceAllString("2.00", "")
+		trailingZerosRegex.ReplaceAllString("2.0", "")
+		trailingZerosRegex.ReplaceAllString("2", "")
+	}
+}
+
+func BenchmarkFtoaFunc(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		stripTrailingZeros("2.00000")
+		stripTrailingZeros("2.0000")
+		stripTrailingZeros("2.000")
+		stripTrailingZeros("2.00")
+		stripTrailingZeros("2.0")
+		stripTrailingZeros("2")
+	}
+}
+
+func BenchmarkFmtF(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fmt.Sprintf("%f", 2.03584)
+	}
+}
+
+func BenchmarkStrconvF(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		strconv.FormatFloat(2.03584, 'f', 6, 64)
+	}
 }
